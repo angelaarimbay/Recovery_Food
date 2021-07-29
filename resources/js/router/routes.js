be5cf047fb195a06882f66289a90c101acf3fada
julius1523 <juliusdolana13@gmail.com>
2021-07-29 10:17:50 +0800

    updates

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -5,11 +5,11 @@ function page (path) {
 export default{
     mode: 'history',
     routes:[ 
-        { path:'*', component: page('errors/page-not-found.vue') }, 
-        { path:'/',  component: page('login.vue') },  
-        { path:'/login', name: 'login', component: page('login.vue') },  
+        // { path:'*', component: page('errors/page-not-found.vue') }, 
+        // { path:'/',  component: page('login.vue') },  
+        { path:'/', name: 'dashboard', component: page('dashboard.vue') },  
         { path:'/dashboard', name: 'dashboard', component: page('dashboard.vue') },  
-
+ 
         { path:'/survey/infosheet', name: 'survey.infosheet', component: page('survey/infosheet.vue') }, 
         { path:'/accumulation', name: 'accumulation', component: page('maps/accumulation.vue') }, 
         { path:'/maps/googlemap', name: 'maps.googlemap', component: page('maps/googlemap.vue') }, 
