d4f452f27ea75e1d2ce0458daf3cef9963fbaf2b
CYRUSM23 <cyrusmabulay@gmail.com>
2021-08-03 15:40:31 +0800

    updates

--- resources/js/components/Nav.vue
+++ resources/js/components/Nav.vue
@@ -1,7 +1,8 @@
 <template>
-  <nav>
-    <v-app-bar app dense dark class="red darken-3">
-      <v-app-bar-nav-icon @click.stop="mini = !mini; "></v-app-bar-nav-icon>
+  <div>
+    <v-app-bar  dense dark class="red darken-3" app>
+      <v-app-bar-nav-icon v-if="$vuetify.breakpoint.mdAndDown" @click.stop="mini = false; drawer = !drawer;"></v-app-bar-nav-icon> 
+      <v-app-bar-nav-icon v-else @click.stop="drawer = true; mini = !mini;"></v-app-bar-nav-icon>
 
       <v-list-item-title class="hidden-sm-and-down"
         >Online Inventory and Sales Monitoring System for Recovery
@@ -28,15 +29,18 @@
         </v-list>
       </v-menu>
     </v-app-bar>
+
+
     <v-navigation-drawer
       height="100%"
       app
-      v-model="drawer"
-      :permanent="$vuetify.breakpoint.smAndUp"
+      v-model="drawer" 
       :mini-variant.sync="mini"
       dark
       class="grey darken-4"
     >
+
+    
       <template v-slot:prepend>
         <v-list-item class="px-2">
           <v-list-item-avatar class="mb-0">
@@ -261,7 +265,7 @@
         </v-list-item>
       </template>
     </v-navigation-drawer>
-  </nav>
+  </div>
 </template>
 
 <style>
@@ -277,7 +281,7 @@
 export default {
   data() {
     return {
-      drawer: false,
+      drawer: true,
       mini: false,
     };
   },
