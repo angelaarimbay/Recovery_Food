31cb135639f4b5c01ec2abd225e03573ca7545eb
CYRUSM23 <cyrusmabulay@gmail.com>
2021-08-16 14:52:59 +0800

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,19 +10,19 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>  
+    <script src="{{ mix('dist/js/app.js') }}" defer></script>  
+    
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.5.95/css/materialdesignicons.css">  
+ 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('dist/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app"> 
             @yield('content') 
-    </div>
-    
-     
+    </div> 
 </body>
 </html>
