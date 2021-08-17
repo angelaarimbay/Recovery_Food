06a2111f3efc04ec257716b91d1f8d1db3b14db2
Bernie Agustin <masipag90@gmail.com>
2021-08-16 23:10:03 -0700

    update

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <p class="text-h6">
-      <v-icon size="30">mdi-view-dashboard</v-icon>Dashboard
+      <v-icon size="30">mdi-view-dashboard</v-icon>Dashboard 1
     </p>
 
     
