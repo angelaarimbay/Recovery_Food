63217b9a12147aac904de2cde6b30d08d31e2011
julius1523 <juliusdolana13@gmail.com>
2021-08-17 11:38:41 +0800

    updates

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <p class="text-h6">
-      <v-icon size="30">mdi-view-dashboard</v-icon>Dashboard
-    </p>
+    <p class="text-h6 bg-success">
+      <v-icon size="30">mdi-view-dashboard</v-icon>Dashboard 12
+      test
 
+    </p>
+  <small>a</small>
+
+  <v-btn>test</v-btn>
     
   </div>
 </template> 
